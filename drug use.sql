44afe5b462c351b320fb17cd31ce392290c7461e
Wingbean <taeey.com@gmail.com>
2025-03-27 15:20:03 +0700

    add druguse

---
+++ drug use.sql
@@ -0,0 +1,15 @@
+SELECT
+o.icode as รหัสยา
+,d.tmt_tp_code as TTMT_code
+,d.name as Drug
+,d.strength 
+,d.units
+,count(o.icode)as list
+,sum(o.qty)as quantity_จำนวนสั่ง
+,d.unitcost as ราคาต่อหน่วย
+,(sum(o.qty)*d.unitcost)as amount_มูลค่า
+from opitemrece o
+join drugitems d on d.icode=o.icode 
+where o.rxdate between "2025-03-27" and "2025-03-27" and d.tmt_tp_code IN(798783,717021,532586,940556,264414,9092983,666847,9374895,486941,715038)
+group by o.icode order by d.name,d.strength 
+;
