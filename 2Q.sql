cfa860e8d87f9fe20c539a530be6f783c32d4e84
Wingbean <taeey.com@gmail.com>
2025-04-02 15:30:12 +0700

    2Q

--- 2Q.sql
+++ 2Q.sql
@@ -10,4 +10,5 @@ from vn_stat v
 JOIN patient p ON p.hn = v.hn
 JOIN pp_special pp  on  pp.vn = v.vn
 JOIN pp_special_type ps  on  ps.pp_special_type_id = pp.pp_special_type_id
-WHERE v.vstdate BETWEEN "2024-04-01" and "2024-04-12" and ps.pp_special_code NOT in ("1B130","1B131","1B0280","1B0281","1B141","1B141");
+WHERE v.vstdate BETWEEN "2024-04-01" and "2024-04-12" and ps.pp_special_code NOT in ("1B130","1B131","1B0280","1B0281","1B141","1B141")
+;
