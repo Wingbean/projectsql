e9929a0aa42dc1bf33a9cfb88e2860913ab034aa
Wingbean <taeey.com@gmail.com>
2025-05-15 08:38:03 +0700

    N delete .sql outside folder scripts

--- AllPt.sql
+++
@@ -1,8 +0,0 @@
-select cid AS CID
-,hn AS HN
-,concat(fname,"  ",lname) AS NAME
-,moopart AS MooHosXP
-,death AS DEATH
-from patient
--- where hn = 45130
-
--- SQL/Script-1.sql
+++
@@ -1,8 +0,0 @@
-SELECT o.icode ,d.name , count( o.vn ) AS vn , sum(o.qty) AS total,d.unitprice
-from opitemrece o
-left outer join drugitems d on d.icode = o.icode
-where o.rxdate BETWEEN "2024-08-01" and "2024-08-01"
-and o.icode like "1%"
-GROUP BY  o.icode
-ORDER BY total  DESC
-#limit 10
